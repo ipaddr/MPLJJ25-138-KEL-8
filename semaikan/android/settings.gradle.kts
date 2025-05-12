9460cfd4b7dfe99bf788faa304e3a8f559c7b1ff
Erpiana02 <erpiana@student.unp.ac.id>
2025-05-12 21:44:24 +0700

    menambahkan firebase
    menambahkan firebase untuk akun dan masuk

--- semaikan/android/settings.gradle.kts
+++ semaikan/android/settings.gradle.kts
@@ -19,7 +19,10 @@ pluginManagement {
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
     id("com.android.application") version "8.7.0" apply false
+    // START: FlutterFire Configuration
+    // END: FlutterFire Configuration
     id("org.jetbrains.kotlin.android") version "1.8.22" apply false
+    id ("com.google.gms.google-services") version "4.4.2" apply false
 }
 
 include(":app")
